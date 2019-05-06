b23f2e35efdee272f1c45bd801f3ab252b440f0d
Carlos <mcarlitos.146@gmail.com>
2019-05-06 17:04:11 +0200

    Scripts lanzador de bolas

---
+++ Final Multimodal/Assets/Municion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Municion : MonoBehaviour {
+
+    public float vel;
+	Vector3 velocidad;
+	public float ToLife=2; 
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Velocidad " + velocidad.x + " " + velocidad.y + " " + velocidad.z);
+		transform.position = transform.position + velocidad*Time.deltaTime;
+	}
+	public void Lanzar(Transform t)
+	{
+		transform.position = t.position;
+		velocidad= new Vector3(0,0,-1 * vel);
+		GameObject.Destroy(gameObject,ToLife);
+
+	}
+
+}
---
+++ Final Multimodal/Assets/Torreta.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torreta : MonoBehaviour
+{
+
+    [Header("Unity Setup Fields")]
+
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    public GameObject municion;
+    public Transform salida_municion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireCountdown <= 0f)
+        {
+            Disparar();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Disparar()
+    {
+        //Rellena el código necesario
+        GameObject bala = (GameObject)Instantiate(municion, salida_municion.transform.position, salida_municion.transform.rotation);
+        Municion bullet = bala.GetComponent<Municion>();
+
+        if (bullet != null)
+        {
+            bullet.Lanzar(salida_municion);
+        }
+    }
+}
