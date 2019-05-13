e73cdab84fec5912f634ea5a9aeebdba192ea836
Carlos <mcarlitos.146@gmail.com>
2019-05-13 16:56:31 +0200

    wawawiwa

---
+++ Final Multimodal/Assets/Golpe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golpe : MonoBehaviour
+{
+    private Vector3 dir;
+    private bool is_triggered =false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 norm = Input.mousePosition.normalized;
+            dir = new Vector3(norm.x, norm.y, 1);
+            is_triggered = true;
+        }
+        else
+        {
+            is_triggered = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (is_triggered)
+        {
+            
+        }
+    }
+}
---
+++ Final Multimodal/Assets/Turret.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    public GameObject municion;
+    public Transform salida;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireCountdown <= 0f)
+        {
+            Disparar();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+
+    }
+    void Disparar()
+    {
+        GameObject Bala = (GameObject)Instantiate(municion, salida.position, salida.rotation);
+        Bala.transform.forward = salida.forward;
+
+    }
+}
