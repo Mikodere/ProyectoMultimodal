d59d67502cb3d7c109e0af2c21586216a5ada9c9
Carlos <mcarlitos.146@gmail.com>
2019-05-21 16:58:46 +0200

    Hit con la pelota implementado

--- Final Multimodal/Assets/Golpe.cs
+++ Final Multimodal/Assets/Golpe.cs
@@ -4,34 +4,51 @@ using UnityEngine;
 
 public class Golpe : MonoBehaviour
 {
+    private Camera cam;
     private Vector3 dir;
-    private bool is_triggered =false;
+    private bool is_triggered = false;
+    private float velocidad;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector2 norm = Input.mousePosition.normalized;
-            dir = new Vector3(norm.x, norm.y, 1);
-            is_triggered = true;
-        }
-        else
-        {
-            is_triggered = false;
+            //pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.position.z);
+            Ray rayo = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit; 
+            Debug.DrawRay(rayo.origin, rayo.direction * 10, Color.yellow);
+
+            dir = new Vector3(-rayo.direction.x, rayo.direction.y, rayo.direction.z);
+            velocidad = Input.acceleration.z;
+
+            if (Physics.Raycast(rayo, out hit))
+            {
+                if(hit.rigidbody != null && is_triggered == true)
+                {
+                    Debug.Log("Hit!" + velocidad);
+                    hit.rigidbody.AddForceAtPosition(dir.normalized*25f, hit.point, ForceMode.Impulse);
+                }
+            }
         }
     }
 
-    void OnTriggerEnter(Collider c)
+    void OnTriggerEnter (Collider c)
     {
-        if (is_triggered)
+        if(c.tag == "pelota")
         {
-            
+            is_triggered = true;
         }
     }
+
+    void OnTriggerExit (Collider c)
+    {
+        is_triggered = false;
+    }
+
 }
