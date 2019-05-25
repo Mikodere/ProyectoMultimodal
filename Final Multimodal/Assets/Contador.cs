00e77dfd0875b68b96dc8b19661662554cdc534a
dominrleon <domingorodriguezle@gmail.com>
2019-05-25 09:25:19 +0200

    Sumador de puntos

--- Final Multimodal/Assets/Contador.cs
+++ Final Multimodal/Assets/Contador.cs
@@ -7,8 +7,9 @@ public class Contador : MonoBehaviour
 {
     private int count;
     public Text countText;
-// Start is called before the first frame update
-void Start()
+
+    // Start is called before the first frame update
+    void Start()
     {
         count = 0;
     }
@@ -16,10 +17,22 @@ void Start()
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void onTriggerEnter(Collider other)
     {
         count++;
+        countText.text = count.ToString()
+    }
+    void SetCountText()
+    {
+        if (countText.text != null)
+        {
+            countText.text = "Count: " + count.ToString();
+        }
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
